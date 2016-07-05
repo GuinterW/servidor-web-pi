7fc4e1a361812fa86b40468ac7a56c2e5f53bf1c
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-05 16:49:39 -0300

    Servidor raspberry pi

--- server-pi/routes/index.js
+++ server-pi/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var wpi = require('wiring-pi');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	
+	
+	var configPin = 7;
+		
+	wpi.setup('wpi');
+	wpi.pinMode(configPin, wpi.OUTPUT);
+
+	wpi.digitalWrite(configPin, 1 );
+	console.log('ligou');
+
+	setTimeOut(function(){
+		wpi.digitalWrite(configPin, 0 );
+		console.log('Desligou');
+	}, 3000);
 });
 
 module.exports = router;
