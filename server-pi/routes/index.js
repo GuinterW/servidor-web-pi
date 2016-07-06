49299c8f668e5b06319576b8797efc43367c3c2f
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-07-06 10:12:08 -0300

    index

--- server-pi/routes/index.js
+++ server-pi/routes/index.js
@@ -4,19 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	
-	var configPin = 7;
-		
-	wpi.setup('wpi');
-	wpi.pinMode(configPin, wpi.OUTPUT);
-
-	wpi.digitalWrite(configPin, 1 );
-
-	setTimeout(function(){
-		wpi.digitalWrite(configPin, 0 );
-	}, 3000);
-
-	res.sendStatus(200);
+	res.render('pi server');
 });
 
 module.exports = router;
