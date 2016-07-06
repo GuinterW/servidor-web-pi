b52d3d56e88300ec3539ea3d933e1ffdd8626c22
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-06 13:51:04 -0300

    Require

--- server-pi/routes/index.js
+++ server-pi/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var wpi = require('wiring-pi');
 var router = express.Router();
-var button = require('./button.js');
+var button = require('./../actions/button/button.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
