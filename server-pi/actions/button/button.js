d757176b2acc26caf85ea3254119f3075d8563d3
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-06 16:19:49 -0300

    Button

--- server-pi/actions/button/button.js
+++ server-pi/actions/button/button.js
@@ -1,47 +1,6 @@
 var wpi = require('wiring-pi');
 var gpio = require('rpi-gpio');
 
-function sendRequest(url,callback,postData) {
-    var req = createXMLHTTPObject();
-    if (!req) return;
-    var method = (postData) ? "POST" : "GET";
-    req.open(method,url,true);
-    req.setRequestHeader('User-Agent','XMLHTTP/1.0');
-    if (postData)
-        req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-    req.onreadystatechange = function () {
-        if (req.readyState != 4) return;
-        if (req.status != 200 && req.status != 304) {
-//          alert('HTTP error ' + req.status);
-            return;
-        }
-        callback(req);
-    }
-    if (req.readyState == 4) return;
-    req.send(postData);
-}
-
-var XMLHttpFactories = [
-    function () {return new XMLHttpRequest()},
-    function () {return new ActiveXObject("Msxml2.XMLHTTP")},
-    function () {return new ActiveXObject("Msxml3.XMLHTTP")},
-    function () {return new ActiveXObject("Microsoft.XMLHTTP")}
-];
-
-function createXMLHTTPObject() {
-    var xmlhttp = false;
-    for (var i=0;i<XMLHttpFactories.length;i++) {
-        try {
-            xmlhttp = XMLHttpFactories[i]();
-        }
-        catch (e) {
-            continue;
-        }
-        break;
-    }
-    return xmlhttp;
-}
-
 function button(){
     var url = "localhost:3000/unlock";
 }
@@ -52,10 +11,12 @@ button.prototype.configInput = function(pin){
 }
 
 button.prototype.read = function(pin){
-    var configTimeout = 1000;
+    var configTimeout = 500;
     setInterval(function() {
         gpio.read(pin, function(err, value) {
-            console.log('The value is ' + value);
+            if(value!==undefined){
+                console.log('The value is ' + value);
+            }
         });
     }, configTimeout);
 }
