19519cea94ab1f300b84f334906352c5b3116768
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-07-06 11:18:21 -0300

    button

---
+++ server-pi/actions/button/button.js
@@ -0,0 +1,22 @@
+function button(){
+    this.wpi = require('wiring-pi'); 
+}
+
+button.prototype.configIntput = function(pin){
+    // GPIO pin of the led
+    var configPin = pin;
+    this.wpi.setup('wpi');
+    this.wpi.pinMode(configPin, this.wpi.INTPUT);
+}
+
+button.prototype.read = function(pin){
+    var configTimeout = 100;
+    setInterval(function() {
+        var status = this.wpi.digitalRead(pin);
+        if(status==1){
+            window.location.href="localhost:3000/unlock";
+        }
+    }, configTimeout);
+}
+
+module.exports = new buttonRead();
--- server-pi/routes/index.js
+++ server-pi/routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var wpi = require('wiring-pi');
 var router = express.Router();
+var button = require('../actions/button/button.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Raspberry PI' });
 });
+
+button.configInput(11);
+button.read(11);
 
 module.exports = router;
