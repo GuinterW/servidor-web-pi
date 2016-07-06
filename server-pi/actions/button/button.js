c44dd339a451752a4dd1ae55aeccadc7fe16aa5d
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-06 16:52:56 -0300

    Button

--- server-pi/actions/button/button.js
+++ server-pi/actions/button/button.js
@@ -22,7 +22,7 @@ button.prototype.write = function(pin, status){
 }
 
 button.prototype.read = function(pin){
-    var configTimeout = 400;
+    var configTimeout = 300;
     setInterval(function() {
         gpio.read(pin, function(err, value) {
             if(value!==undefined){
