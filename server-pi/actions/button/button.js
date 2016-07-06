e8a9cd5c58ddd4eba8d5b0986c097b2490b9e2dd
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-06 14:06:35 -0300

    Button

--- server-pi/actions/button/button.js
+++ server-pi/actions/button/button.js
@@ -1,18 +1,20 @@
+var wpi = require('wiring-pi');
+
 function button(){
-    this.wpi = require('wiring-pi'); 
+     
 }
 
 button.prototype.configInput = function(pin){
     // GPIO pin of the led
     var configPin = pin;
-    this.wpi.setup('wpi');
-    this.wpi.pinMode(configPin, this.wpi.modes.INPUT);
+    wpi.setup('wpi');
+    wpi.pinMode(configPin, wpi.modes.INPUT);
 }
 
 button.prototype.read = function(pin){
     var configTimeout = 100;
     setInterval(function() {
-        var status = this.wpi.digitalRead(pin);
+        var status = wpi.digitalRead(pin);
         if(status==1){
             window.location.href="localhost:3000/unlock";
         }
