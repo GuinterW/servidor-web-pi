315977a0bb9b0b09a29de3a7f2b0291978bd841e
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-06 16:28:06 -0300

    Button

--- server-pi/actions/button/button.js
+++ server-pi/actions/button/button.js
@@ -10,12 +10,30 @@ button.prototype.configInput = function(pin){
     gpio.setup(pin, gpio.DIR_IN, this.read);
 }
 
+button.prototype.configOutput = function(pin){
+    var configPin = pin;
+
+    wpi.setup('wpi');
+    wpi.pinMode(configPin, wpi.OUTPUT);
+}
+
+button.prototype.write = function(pin, status){
+    wpi.digitalWrite(pin, status);
+}
+
 button.prototype.read = function(pin){
     var configTimeout = 500;
     setInterval(function() {
         gpio.read(pin, function(err, value) {
             if(value!==undefined){
                 console.log('The value is ' + value);
+            }
+            if(value==true){
+                this.write(7, 1);
+
+                setTimeout(function(){
+                    this.write(7, 0);
+                }, 3000);
             }
         });
     }, configTimeout);
--- server-pi/routes/unlock.js
+++ server-pi/routes/unlock.js
@@ -1,20 +1,17 @@
 var express = require('express');
 var wpi = require('wiring-pi');
+var button = require('./../actions/button/button.js');
 var router = express.Router();
 
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-	
-	var configPin = 7;
-		
-	wpi.setup('wpi');
-	wpi.pinMode(configPin, wpi.OUTPUT);
 
-	wpi.digitalWrite(configPin, 1);
+	button.configOutput(7);
+	button.write(7, 1);
 
 	setTimeout(function(){
-		wpi.digitalWrite(configPin, 0);
+		button.write(7, 0);
 	}, 3000);
 
 	res.sendStatus(200);
