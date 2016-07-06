9c1fd47ceaaf6a996ce77c1b1de0bce00daa01cd
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-06 14:27:20 -0300

    Button

--- server-pi/routes/unlock.js
+++ server-pi/routes/unlock.js
@@ -4,20 +4,21 @@ var router = express.Router();
 
 /* GET home page. */
 module.exports = {
-
-	unlock : function() {
+	router.get(function(req, res){
+		unlock : function() {
 		
-		var configPin = 7;
-			
-		wpi.setup('wpi');
-		wpi.pinMode(configPin, wpi.OUTPUT);
+			var configPin = 7;
+				
+			wpi.setup('wpi');
+			wpi.pinMode(configPin, wpi.OUTPUT);
 
-		wpi.digitalWrite(configPin, 1);
+			wpi.digitalWrite(configPin, 1);
 
-		setTimeout(function(){
-			wpi.digitalWrite(configPin, 0);
-		}, 3000);
+			setTimeout(function(){
+				wpi.digitalWrite(configPin, 0);
+			}, 3000);
 
-		res.sendStatus(200);
-	}
+			res.sendStatus(200);
+		}
+	});
 };
