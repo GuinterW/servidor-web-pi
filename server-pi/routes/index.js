b0a24039ead0004d21063c8b7c7aa8f01c190e69
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-06 10:44:24 -0300

    Index

--- server-pi/routes/index.js
+++ server-pi/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('pi server');
+  res.render('index', { title: 'Raspberry PI' });
 });
 
 module.exports = router;
