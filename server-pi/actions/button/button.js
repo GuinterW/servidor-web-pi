d7fd7c2430597961a790e46d83bd385a7736704d
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-07 11:04:08 -0300

    Biblioteca rpi-gpio

--- server-pi/actions/button/button.js
+++ server-pi/actions/button/button.js
@@ -17,7 +17,7 @@ button.prototype.write = function(pin, status){
 }
 
 button.prototype.read = function(pin){
-    var configTimeout = 250;
+    var configTimeout = 300;
     setInterval(function() {
         gpio.read(pin, function(err, value) {
             if(value!==undefined){
@@ -25,10 +25,16 @@ button.prototype.read = function(pin){
             }
             if(value==true){
                 var configPin = 7;
-                gpio.on('change', function(channel, value) {
-                    console.log('Channel ' + channel + ' value is now ' + value);
+                gpio.setup(configPin, gpio.DIR_OUT);
+                gpio.write(configPin, true, function(err){
+                    setTimeout(function(){
+                        gpio.write(configPin, false);
+                    }, 1000);
+
+                    setTimeout(function(){
+                        gpio.write(configPin, true);
+                    }, 1000);
                 });
-                gpio.setup(configPin, gpio.DIR_OUT, gpio.EDGE_BOTH);
             }
         });
     }, configTimeout);
