b4be3b932a311ca5f05eb5c1b979aefb6f668623
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-07 13:48:29 -0300

    Biblioteca rpi-gpio

--- server-pi/actions/button/button.js
+++ server-pi/actions/button/button.js
@@ -24,29 +24,26 @@ button.prototype.read = function(pin, callback){
 }
 
 button.prototype.unlock = function(){
-    var configInterval = 300;
-    setInterval(function() {
-        gpio.read(18, function(err, value){
-            /*if(value!==undefined){
-                console.log('The value is : ' + value);
-            }*/
-            if(value==true){
-                var configPin = 7;
-                /*this.configInput(configPin, function(err){
-                    this.write(configPin, true, function(err){
-                        this.delayWrite(configPin, false, 3000);
-                    });
-                });*/
-                gpio.setup(configPin, gpio.DIR_OUT, function(err){
-                    gpio.write(configPin, true, function(err){
-                        setTimeout(function(){
-                            gpio.write(configPin, false);
-                        }, 3000);
-                    });
+    this.read(18, function(err, value){
+        /*if(value!==undefined){
+            console.log('The value is : ' + value);
+        }*/
+        if(value==true){
+            var configPin = 7;
+            /*this.configInput(configPin, function(err){
+                this.write(configPin, true, function(err){
+                    this.delayWrite(configPin, false, 3000);
                 });
-            }
-        });
-    }, configInterval);
+            });*/
+            gpio.setup(configPin, gpio.DIR_OUT, function(err){
+                gpio.write(configPin, true, function(err){
+                    setTimeout(function(){
+                        gpio.write(configPin, false);
+                    }, 3000);
+                });
+            });
+        }
+    });
 }
 
 button.prototype.delayWrite = function(pin, status, time){
