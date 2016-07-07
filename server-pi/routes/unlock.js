669e0e8f80a8539e1f7a10f77772b3b572c72f57
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-07 15:28:37 -0300

    Histórioco com data e hora

--- server-pi/routes/unlock.js
+++ server-pi/routes/unlock.js
@@ -6,9 +6,20 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
-	button.unlock();
-
-	res.sendStatus(200);
+	gpio.setup(configPin, gpio.DIR_OUT, function(err){
+	    gpio.write(configPin, true, function(err){
+	        var now = new Date();  
+	        var dia = now.getDate();
+	        var mes = now.getMonth()+1;
+	        var ano = now.getFullYear();
+	        var hora = '['+now.getHours()+':'+now.getMinutes()+':'+now.getSeconds()+']';
+	        var data = 'Ativado em: '+dia+'/'+mes+'/'+ano+' - '+hora;
+	        console.log(data);
+	        setTimeout(function(){
+	            gpio.write(configPin, false);
+	        }, 3000);
+	    });
+	});
 });
 
 module.exports = router;
