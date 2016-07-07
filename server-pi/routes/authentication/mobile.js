070271a15af1bb6e9aa82da208fd932b2a46516c
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-07 16:43:29 -0300

    mobile authentication

--- server-pi/routes/authentication/mobile.js
+++ server-pi/routes/authentication/mobile.js
@@ -3,23 +3,23 @@ var Senha = express.Router();
 //var Reserva = getmodule('api/api');
 
 //CONEXAO SERVER.
-/*var mysql = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : '192.168.1.140',
     user     : 'root',
     password : 'grenal23',
     port : 3306, //port mysql
     database:'servidorPi'
-});*/
+});
 
 
 Senha.get('/', function(req, res){
     var senha= req.query.senha;
-    /*connection.query('SELECT  *  FROM  usuarios WHERE chave LIKE ?', ['%' + senha], function(err,result){
+    connection.query('SELECT  *  FROM  usuarios WHERE chave LIKE ?', ['%' + senha], function(err,result){
         res.type('json');
         //res.redirect('../unlock');
         res.sendStatus(200);
-    });*/
+    });
     res.sendStatus(200);
 });
 
