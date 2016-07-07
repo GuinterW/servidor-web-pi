99c586022a823717a8fc3c7a1d7d48a38ee6f0b7
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-07-07 15:51:06 -0300

    mobile authentication

---
+++ server-pi/routes/authentication/mobile.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var Senha = express.Router();
+//var Reserva = getmodule('api/api');
+
+//CONEXAO SERVER.
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'grenal23',
+    port : 3306, //port mysql
+    database:'servidorPi'
+});
+
+
+Senha.get('/:senha', function(req, res){
+    var senha= req.params.senha;
+    connection.query('SELECT  *  FROM  usuarios WHERE chave LIKE ?', ['%' + senha], function(err,result){
+        res.type('json');
+        res.redirect('../unlock');
+    });
+});
+
+module.exports = Senha;
