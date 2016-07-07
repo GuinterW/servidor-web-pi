7b5646405f435896be65dc0b1b951a0779fc53c6
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-07 15:13:35 -0300

    Histórioco com data e hora

--- server-pi/actions/button/button.js
+++ server-pi/actions/button/button.js
@@ -41,7 +41,7 @@ button.prototype.unlock = function(){
             gpio.setup(configPin, gpio.DIR_OUT, function(err){
                 gpio.write(configPin, true, function(err){
                     var now = new Date();  
-                    var dia = now.getDay();
+                    var dia = now.getDate();
                     var mes = now.getMonth()+1;
                     var ano = now.getFullYear();
                     var hora = '['+now.getHours()+':'+now.getMinutes()+':'+now.getSeconds()+']';
