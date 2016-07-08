884f356a8175c363ab24f4cddd5944d6a05ec540
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-08 14:39:20 -0300

    mongo

--- server-pi/routes/authentication/mobile.js
+++ server-pi/routes/authentication/mobile.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var Senha = express.Router();
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/test';
+//var MongoClient = require('mongodb').MongoClient;
+//var assert = require('assert');
+//var ObjectId = require('mongodb').ObjectID;
+//var url = 'mongodb://localhost:27017/test';
 
 //var connection = require('./../../database_connection/connection.js');
 //var Reserva = getmodule('api/api');
@@ -21,9 +21,10 @@ var url = 'mongodb://localhost:27017/test';
     res.type('json');
     res.redirect('../routes/unlock');
 }*/
-
+/*
 var insertDocument = function(db, callback) {
    db.collection('authentication').insertOne( {
+    "_id" : ObjectId("54c955492b7c8eb21818bd09"),
     "passwords": ["123456", "654321"],
     "mobile": [],
     "nfc": []
@@ -33,7 +34,7 @@ var insertDocument = function(db, callback) {
     callback();
   });
 };
-
+*/
 Senha.get('/', function(req, res){
     var senha= req.query.senha;
     //connection('SELECT  *  FROM  usuarios WHERE chave LIKE ?', ['%' + senha], foo);
@@ -44,14 +45,14 @@ Senha.get('/', function(req, res){
     /*if(senha=='123456'){
         res.redirect('../unlock');
     }*/
-
+/*
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   insertDocument(db, function() {
       db.close();
   });
 });
-
+*/
 
 });
 
