a87fa5e68670bb3ac19f5182508e4b55eb6f36e1
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-11 14:11:03 -0300

    Mongo

--- server-pi/routes/authentication/mobile.js
+++ server-pi/routes/authentication/mobile.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var Senha = express.Router();
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://192.168.1.117:27017/test';
+//var MongoClient = require('mongodb').MongoClient;
+//var assert = require('assert');
+//var ObjectId = require('mongodb').ObjectID;
+//var url = 'mongodb://localhost:27017/test';
 
+//var connection = require('./../../database_connection/connection.js');
+//var Reserva = getmodule('api/api');
+
+//CONEXAO SERVER.
+//var mysql = require('mysql');
+/*var connection = mysql.createConnection({
+    host     : '192.168.1.140',
+    user     : 'root',
+    password : 'grenal23',
+    port : 3306, //port mysql
+    database:'servidorPi'
+});*/
+/*function foo(){
+    res.type('json');
+    res.redirect('../routes/unlock');
+}*/
 /*
 var insertDocument = function(db, callback) {
-   db.collection('restaurants').insertOne( {
+   db.collection('authentication').insertOne( {
     "_id" : ObjectId("54c955492b7c8eb21818bd09"),
     "passwords": ["123456", "654321"],
     "mobile": [],
@@ -19,17 +35,25 @@ var insertDocument = function(db, callback) {
   });
 };
 */
-
-//res.redirect('../unlock');
-
 Senha.get('/', function(req, res){
-  var senha= req.query.senha;
-/*  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    insertDocument(db, function() {
+    var senha= req.query.senha;
+    //connection('SELECT  *  FROM  usuarios WHERE chave LIKE ?', ['%' + senha], foo);
+    /*connection.query('SELECT  *  FROM  usuarios WHERE chave LIKE ?', ['%' + senha], function(err,result){
+        res.type('json');
+        //res.redirect('../unlock');
+    });*/
+    /*if(senha=='123456'){
+        res.redirect('../unlock');
+    }*/
+/*
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  insertDocument(db, function() {
       db.close();
-    });
-  });*/
+  });
+});
+*/
+
 });
 
 module.exports = Senha;
