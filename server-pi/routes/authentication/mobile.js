3bb6009dd622fe2ed60982509a0a378187d6cc8d
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-12 14:33:17 -0300

    Key unlock

--- server-pi/routes/authentication/mobile.js
+++ server-pi/routes/authentication/mobile.js
@@ -5,7 +5,7 @@ var Mongo = require("./../../classes/mongo/mongo.js");
 Senha.get('/', function(req, res){
     var senha= req.query.senha;
     Mongo.find({key:senha}, res, function(res){
-        res.redirect("./../unlock?key=fjdn!=?fuioeawuifpEJP9!=?3P98JEWAD9V32H9F8Nn?f9Q3NF9UHQ3HIu2qh!eh2qHE92HQUE==2W");
+        res.redirect("./../unlock?key=fjdn!=?fuio9==2W");
     });
 });
 
