cdfaab05655ee67568ab6e839d1e2a26719b6aaa
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-07-12 11:24:27 -0300

    mongo

--- server-pi/routes/index.js
+++ server-pi/routes/index.js
@@ -4,19 +4,19 @@ var led = require('./../actions/led.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Raspberry PI' });
+  	res.render('index', { title: 'Raspberry PI' });
 });
 
 var Gpio = require('onoff').Gpio,
     button = new Gpio(24, 'in', 'rising');
 
 button.watch(function(err, value){
-  if (err) {
-    throw err;
-  }
-  if(value==1){
-    led();
-  }
+  	if (err) {
+    	throw err;
+  	}
+  	if(value==1){
+    	led();
+  	}
 });
 
 module.exports = router;
