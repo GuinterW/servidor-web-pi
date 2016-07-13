e35a2db3cbc03b85d0098634bf3bd53fbd29c228
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-13 16:49:50 -0300

    Button

--- actions/lock.js
+++ actions/lock.js
@@ -1,9 +1,11 @@
 module.exports = function(){
 	var Gpio = require('onoff').Gpio,
     	lock = new Gpio(20, 'out'),
-    	vcc = new Gpio(26, 'out');
+    	vcc = new Gpio(26, 'out'),
+    	rele = new Gpio(16, 'out');
     
     vcc.writeSync(1);
+    rele.writeSync(1);
 	lock.writeSync(1);
 	setTimeout(function(){
 		lock.writeSync(0);
