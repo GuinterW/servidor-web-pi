08c1cccc2a746be6e514fab04a1e8075c152b4c9
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-14 11:20:12 -0300

    Test Sound

--- actions/sound.js
+++ actions/sound.js
@@ -1,14 +1,17 @@
 module.exports = function(){
+	
 	var Sound = require('node-aplay');
-
 	var music = new Sound('/home/pi/Music/som.wav');
+
 	music.play();
 
-	setTimeout(function () {
+	setTimeout(function(){
 		music.pause();
 	}, 1000);
-	 
-	setTimeout(function () {
+	
+	/*
+	setTimeout(function(){
 		music.resume();
 	}, 3000);
+	*/
 }
