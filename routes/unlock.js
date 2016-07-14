0bfd2971d3fa25faf794e42dd9db422eb0e71bd6
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-14 10:50:29 -0300

    Test Sound

--- routes/unlock.js
+++ routes/unlock.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var lock = require('./../actions/lock.js');
+var sound = require('./../actions/sound.js');
 var gpio = require('rpi-gpio');
 
 router.get('/', function(req, res, next) {
     if(req.query.key=='DD2016TRNEE'){
         lock();
+        sound();
         res.sendStatus(200);
     }
     else {
