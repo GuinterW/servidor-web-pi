1045dba1ca904cf3a98022083dd6b3b0c796063b
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-14 13:28:23 -0300

    Button

--- actions/lock.js
+++ actions/lock.js
@@ -1,13 +1,16 @@
 module.exports = function(device, user){
     var sound = require('./sound.js');
     var mongo = require('./../classes/mongo/mongo.js');
+    
     var now = new Date();  
     var dia = now.getDate();
     var mes = now.getMonth()+1;
     var ano = now.getFullYear();
     var hora = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
     var data = dia+'/'+mes+'/'+ano+' - '+hora;
+    
     var insertObj = {dateTime: data, user: user, device: device};
+    
     var Gpio = require('onoff').Gpio,
         lock = new Gpio(20, 'out'),
         vcc = new Gpio(26, 'out'),
