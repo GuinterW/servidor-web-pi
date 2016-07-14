3010cebddedd8edbe82ec7a18407f5f16e155092
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-14 11:28:26 -0300

    Test Sound

--- actions/sound.js
+++ actions/sound.js
@@ -1,6 +1,6 @@
 module.exports = function(){
 	var Sound = require('node-aplay');
-	var music = new Sound('/home/pi/Music/som.wav');
+	var music = new Sound('../media/dingdong.wav');
 
 	music.play();
 
